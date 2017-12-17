bf7b0d74b42fcd2ff7db69a39cd38db80ce7b162
Hassan Mir <hassanmir92@gmail.com>
2017-12-17 22:24:05 +0000

    Messages view layout
    * Form to submit a new message.
    * List to display messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,20 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+
+    @messages = Message.all
   end
 
   def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      flash[:success] = "Message successfully created!"
+    else
+      flash[:error] = @message.errors.values.join(", ")
+    end
+
+    redirect_to root_path
   end
 
   private
