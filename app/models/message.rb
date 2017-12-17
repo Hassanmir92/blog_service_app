f9edfbdc60566e6f4dc0fa78c8562217b0deed62
Hassan Mir <hassanmir92@gmail.com>
2017-12-17 22:24:39 +0000

    Message validation
    * Message validation to ensure presence of body and a maximum length of 150 characters

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,4 @@
 class Message < ActiveRecord::Base
+  validates_presence_of :body, message: "Unsuccessful to submit as your message is empty!"
+  validates :body, length: { maximum: 150, message: "Unsuccessful to submit your message cannot exceed 150 characters!"}
 end
